06e4960bd20e1367e4a8b5f07eb7ec40ec7b4b76
LegrandThomas <pro.legrand.thomas@gmail.com>
2022-10-19 12:25:59 +0200

    migration model seeder

--- src/db/config.ts
+++ src/db/config.ts
@@ -4,20 +4,9 @@ import { Users } from "../models/users";
 import { contact_form } from "../models/mailing";
 require('dotenv').config()
 
-// const connection = new Sequelize({
-//   dialect: "mysql",
-//   host: process.env.HOST,
-//   username: process.env.USER,
-//   password: process.env.PASSWORD,
-//   database: process.env.DB,
-//   logging: false,
-//   models: [Todos,Users,contact_form],
-// });
-
-
 // export default connection;
 export const connection =new Sequelize('gtbuqdkd','gtbuqdkd','P3FrZufG7CUy5lmuzdwuiTcCD_77scrO',{
-    host:'mouse.db.elephantsql.com',
+    host:process.env.hostpg,
     dialect:'postgres',
     logging: false,
     models: [Todos,Users,contact_form],
